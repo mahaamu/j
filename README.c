c0e587e13d7d8fdf8c143355b6d553400c13b3ef
mahaamu <mahalakshmi3901@gmail.com>
2016-07-16 14:56:23 +0530

    Update and rename README.md to README.c
    finish

---
+++ README.c
@@ -0,0 +1,20 @@
+# j
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,sum=0,ld;
+    cin>>n;
+    int temp=n;
+    while(temp!=0){
+        ld=temp%10;
+        sum=sum*10 +ld;
+        temp/=10;
+    }
+    if(n == sum)
+        cout<<"Palindrome";
+    else
+        cout<<"Not palindrome";
+    return 0;        
+}
